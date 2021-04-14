32638e81b6b5dbaf2930dc62ce9b62dece802891
Drew Hyatt <drew@cicayda.com>
2021-04-14 15:19:01 -0500

    Update config.py for docker mysql

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
 	SECRET_KEY 						= 'myprecious'
-	SQLALCHEMY_DATABASE_URI 		= 'mysql://root:''@localhost/builtapi'
-	# SQLALCHEMY_DATABASE_URI 		= 'mysql://drew:secret@db/builtapi'
+	# SQLALCHEMY_DATABASE_URI 		= 'mysql://root:''@localhost/builtapi'
+	SQLALCHEMY_DATABASE_URI 		= 'mysql://drew:secret@db/builtapi'
 	SQLALCHEMY_TRACK_MODIFICATIONS 	= False
 	KINESIS_DATA_STREAM 			= 'TestStream'
 	AWS_REGION 						= 'us-east-1'
